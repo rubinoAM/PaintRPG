a8e0eec3ba8bb22fe6dc9804aefbae2a0160efe8
Sebastian Price <skphoopa@yahoo.com>
2018-11-19 10:18:56 -0500

    V1.12

--- textrpg.py
+++ textrpg.py
@@ -13,7 +13,7 @@ player_name = input("Pray thee. What is thine name, oh brave adventurer? > ")
 hero['Name'] = player_name.capitalize()
 
 print ("Good day to you, valiant %s! May thine conquest be bountiful and may thine travels be safe." % hero['Name'])
-print ("On thine travels through the kingdom, thou happen upon a giant.")
+print ("On thine travels through the kingdom, thou should happen upon a goblin.")
 print ("What shalt thou do? \n1.Fight\n2.Flee\n3.Dance")
 
 choice = int(input(">"))
@@ -31,7 +31,22 @@ def fight():
         print ("The goblin has %d health and %d power." % (goblin_health, goblin_power))
         print ("What shalt thou do? \n1.Fight\n2.Flee\n3.Dance")
 
-        choice = int(input(">"))
+        choice = int(input("> "))
+
+        if choice == 1:
+            goblin_health -= hero_power
+            print ("You have done %d damage to the goblin!" % hero_power)
+        elif choice == 2:
+            print ("Goodbye, %s... you cowardly sod!" % hero['Name'])
+            break;
+        elif choice == 3:
+            hero_health += 3
+            print ("""The goblin stares dazed and confused at your spontaneous dance.
+            His delayed assault allows you time to regain 3 health points.""")
+            print ("Your health is now at %d points." % hero_health)
+        else:
+            print ("To the beasts with this asinine foolishness! Thou must choose!\n1.Fight\n2.Flee\n3.Dance")
+
 
 
 def flee():
@@ -48,4 +63,4 @@ elif(choice == 3):
     dance()
 else:
     print ("That's not a choice! Pick an actual option!\n1.Fight\n2.Flee\n3.Dance")
-    choice = int(input(">"))
+    choice = int(input("> "))
